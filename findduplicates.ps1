b6849186e8c51c7d5781615fbb51c6687d6d1d6e
svetoslavab <85322530+svetoslavab@users.noreply.github.com>
2021-06-03 20:06:40 -0500

    Add files via upload

---
+++ findduplicates.ps1
@@ -0,0 +1,25 @@
+﻿
+function Find-FirstDuplicateChar{
+
+param([string[]]$string )
+
+$bstr=$string.ToCharArray()
+
+$empty= New-Object -TypeName "System.Collections.ArrayList"
+
+foreach($s in $bstr){ if( $empty -contains $s ){break}
+else{$empty.Add($s) | Out-Null } 
+}
+
+return $s;
+}
+
+
+
+
+
+
+
+
+
+
