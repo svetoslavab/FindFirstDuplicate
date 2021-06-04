b5668c20bdd66f606082133ca25779c8603f1a8c
svetoslavab <85322530+svetoslavab@users.noreply.github.com>
2021-06-03 20:18:10 -0500

    Update findduplicates.ps1
    This is the PowerShell function to find the first duplicate character in a string.

--- findduplicates.ps1
+++ findduplicates.ps1
@@ -1,12 +1,14 @@
-﻿
 function Find-FirstDuplicateChar{
 
 param([string[]]$string )
 
+#Convert the string to array
 $bstr=$string.ToCharArray()
 
+#Create empty array
 $empty= New-Object -TypeName "System.Collections.ArrayList"
 
+#Check if the current value of the cycle is in the $empty array if it is not add it to $empty. The first found is the first duplicated character.
 foreach($s in $bstr){ if( $empty -contains $s ){break}
 else{$empty.Add($s) | Out-Null } 
 }
